2e6f08a81096bfe05eea2d71a5ad0dd838b881f2
Gustavo Allen <gustavoallen92@gmail.com>
2021-07-30 03:10:54 -0400

    project done

--- src/script.js
+++ src/script.js
@@ -3,6 +3,10 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
 
+//Loading
+const textureLoader = new THREE.TextureLoader()
+const normalTexture = textureLoader.load('/textures/height2NormalMap.png')
+
 // Debug
 const gui = new dat.GUI()
 
@@ -13,12 +17,15 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+const geometry =new THREE.SphereBufferGeometry(.5, 64, 64)
 
 // Materials
 
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+const material = new THREE.MeshStandardMaterial()
+material.metalness = .7
+material.roughness = .2
+material.normalMap = normalTexture
+material.color = new THREE.Color(0x292929)
 
 // Mesh
 const sphere = new THREE.Mesh(geometry,material)
@@ -30,7 +37,55 @@ const pointLight = new THREE.PointLight(0xffffff, 0.1)
 pointLight.position.x = 2
 pointLight.position.y = 3
 pointLight.position.z = 4
-scene.add(pointLight)
+// scene.add(pointLight)
+
+//light 2
+const pointLight2 = new THREE.PointLight(0x87ff, 2)
+pointLight2.position.set(2.04, 3, .37)
+pointLight2.intensity = 2.36
+
+scene.add(pointLight2)
+const light2 = gui.addFolder('light 1')
+
+light2.add(pointLight2.position, 'x').min(-6).max(6).step(0.01)
+light2.add(pointLight2.position, 'y').min(-3).max(3).step(0.01)
+light2.add(pointLight2.position, 'z').min(-3).max(3).step(0.01)
+light2.add(pointLight2, 'intensity').min(0).max(10).step(.01)
+
+const light2Color = {
+    color: 0x87ff
+}
+
+light2.addColor(light2Color, 'color').onChange(() => {
+    pointLight2.color.set(light2Color.color)
+})
+
+// const pointLightHelper2 = new THREE.PointLightHelper(pointLight2, .3)
+// scene.add(pointLightHelper2)
+
+//light 3
+const pointLight3 = new THREE.PointLight(0xff0000, 2)
+pointLight3.position.set(-3.29, -3, -.54)
+pointLight3.intensity = 2.8
+
+scene.add(pointLight3)
+const light3 = gui.addFolder('light 2')
+
+light3.add(pointLight3.position, 'x').min(-6).max(6).step(0.01)
+light3.add(pointLight3.position, 'y').min(-3).max(3).step(0.01)
+light3.add(pointLight3.position, 'z').min(-3).max(3).step(0.01)
+light3.add(pointLight3, 'intensity').min(0).max(10).step(.01)
+
+const light3Color = {
+    color: 0xff0000
+}
+
+light3.addColor(light3Color, 'color').onChange(() => {
+    pointLight3.color.set(light3Color.color)
+})
+
+// const pointLightHelper3 = new THREE.PointLightHelper(pointLight3, .3)
+// scene.add(pointLightHelper3) 
 
 /**
  * Sizes
@@ -73,7 +128,8 @@ scene.add(camera)
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    alpha: true
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -82,15 +138,43 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  * Animate
  */
 
+document.addEventListener('mousemove', onDocumentMouseMove)
+
+let mouseX = 0
+let mouseY = 0
+
+let targetX = 0
+let targetY = 0
+
+const windowHalfX = window.innerWidth / 2
+const windowHalfY = window.innerHeight / 2
+
+function onDocumentMouseMove(event) {
+    mouseX = (event.clientX - windowHalfX)
+    mouseY = (event.clientY - windowHalfY)
+}
+
+const updateSphere = (event) => {
+    sphere.position.y = window.scrollY * .001
+}
+
+window.addEventListener('scroll', updateSphere)
+
 const clock = new THREE.Clock()
 
 const tick = () =>
 {
+    targetX = mouseX * .001
+    targetY = mouseY * .001
 
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
     sphere.rotation.y = .5 * elapsedTime
+
+    sphere.rotation.y += .5 * (targetX - sphere.rotation.y)
+    sphere.rotation.x += .05 * (targetY - sphere.rotation.x)
+    sphere.position.z += -.05 * (targetY - sphere.rotation.x)
 
     // Update Orbital Controls
     // controls.update()
